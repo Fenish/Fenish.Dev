936b70c56012401aff07b63d130ca5e32e2599d2
fenish <sohretalhadev@gmail.com>
2023-05-06 00:25:35 +0300

    Initialize Repository

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+useHead(() => ({
+  title: "Fenish.DEV",
+  meta: [
+    {
+      name: "description",
+      content: "Fenish.DEV - Developer",
+    },
+  ],
+  htmlAttrs: {
+    lang: "en",
+  },
+}));
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen bg-gray-200">
+    <div class="container">
+      <div class="bg-white rounded-lg shadow-lg p-5 md:p-20 mx-2">
+        <div class="text-center">
+          <h2
+            class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+          >
+            Fenish.<span class="text-indigo-600">DEV</span>
+          </h2>
+          <h3 class="text-xl md:text-xl mt-10">Under Construction</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
