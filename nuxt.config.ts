013601c8cbacf11e0f6054bb09e1b47ced443782
fenish <sohretalhadev@gmail.com>
2023-05-21 04:32:50 +0300

    Add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss'
-    ],
-    components: true,
-    tailwindcss: {
-        cssPath: '~/assets/css/tailwind.css',
-        viewer: true
-    }
-})
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  components: true,
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    viewer: true,
+  },
+});
