cb850496866ceae0794a51a7dee61c519b43363f
fenish <sohretalhadev@gmail.com>
2023-05-22 03:50:55 +0300

    Add shades of black

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
-  theme: {},
-  plugins: [],
-  darkMode: 'class',
-}
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000000",
+          50: "#202020",
+          100: "#151515",
+          200: "#101010",
+          300: "#050505",
+        },
+      },
+    },
+    plugins: [],
+    darkMode: "class",
+  },
+};
