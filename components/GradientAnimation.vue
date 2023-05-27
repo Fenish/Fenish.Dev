264d7aaf78ccbf6e9b81a8b08de71690e066e507
fenish <sohretalhadev@gmail.com>
2023-05-27 22:49:01 +0300

    Add gradient animation

---
+++ components/GradientAnimation.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps({
+  color: {
+    type: String,
+    required: true,
+    default: "rgba(255, 255, 255, 0.1)",
+  },
+});
+
+const parent = ref<HTMLElement | null>(null);
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 360);
+}
+
+let rotation = generateRandomNumber();
+let randomMargin = generateRandomNumber();
+const speedFactorRandom = Math.random() * 0.4 - 0.2;
+const rotationSpeedFactor = speedFactorRandom;
+function animate() {
+  rotation += rotationSpeedFactor;
+  parent.value!.style.transform = `rotate(${rotation}deg)`;
+  requestAnimationFrame(animate);
+}
+onMounted(() => {
+  parent.value!.style.margin = `${randomMargin}px`;
+  parent.value!.style.transform = `rotate(${rotation}deg)`;
+  animate();
+});
+</script>
+
+<template>
+  <div class="w-full h-full absolute pointer-events-none" ref="parent">
+    <div
+      class="absolute w-64 h-64 blur-3xl top-1/2 -translate-y-1/2"
+      :style="{ backgroundColor: color }"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@keyframes spin-animation {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.random-spin-animation {
+  animation: spin-animation 3s linear;
+}
+</style>
--- components/Panel.vue
+++ components/Panel.vue
@@ -1,40 +1,105 @@
 <template>
   <div
-    ref="parentDiv"
-    @mousemove="handleMouseMove"
-    @mouseleave="hideGlow"
-    @mouseenter="showGlow"
-    class="bg-[#060606] relative flex overflow-hidden justify-center items-center p-1 rounded-md"
+    class="bg-purple-900/30 relative flex overflow-hidden justify-center items-center p-0.5 rounded-md"
   >
-    <Glow
-      :glowLeft="glowLeft"
-      :glowTop="glowTop"
-      :glow-visibility="glowVisibility"
+    <div
+      class="absolute left-0 top-0 w-10 h-10 bg-white blur-2xl border-animation"
     />
-    <div class="bg-black-300 relative flex rounded-md">
-      <span class="text-white px-10 py-10">Hello, I am Talha. 👋</span>
+    <div class="bg-black-300 relative flex rounded-md text-white h-96">
+      <GradientAnimation />
+      <GradientAnimation color="rgba(167, 0, 255, 0.2)" />
+
+      <div class="px-20 py-20">
+        <div class="flex">
+          <div class="w-14 h-14 relative overflow-hidden rounded-full">
+            <img
+              draggable="false"
+              src="../assets/img/avatar-1.png"
+              class="absolute top-11 left-1 scale-150 select-none"
+            />
+          </div>
+          <div class="flex flex-col ml-5">
+            <div>
+              <span class="text-white text-3xl">Hi, I am Talha.</span>
+              <span class="text-3xl hand-shake absolute">👋</span>
+            </div>
+
+            <span class="text-zinc-500">Full stack developer</span>
+          </div>
+        </div>
+
+        <p class="max-w-xl mt-12 text-gray-300">
+          Full-stack developer with a passion for backend development.
+          Proficient in HTML, CSS, JavaScript, and backend technologies. Skilled
+          in designing visually appealing websites and focused on delivering
+          seamless user experiences. Let's collaborate to bring your digital
+          projects to life!
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-const glowTop = ref(0);
-const glowLeft = ref(0);
-const glowVisibility = ref(false);
-const parentDiv = ref(null);
+<style scoped lang="scss">
+.border-animation {
+  animation: border-animation 10s linear infinite;
+}
 
-function hideGlow() {
-  glowVisibility.value = false;
+.hand-shake {
+  cursor: default;
 }
-function showGlow() {
-  glowVisibility.value = true;
+.hand-shake:hover {
+  animation-name: wave-animation;
+  animation-duration: 2.5s;
+  transform-origin: 70% 70%;
 }
-function handleMouseMove(event) {
-  const parentRect = parentDiv.value.getBoundingClientRect();
-  const mouseX = event.clientX - parentRect.left;
-  const mouseY = event.clientY - parentRect.top;
+@keyframes wave-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  10% {
+    transform: rotate(14deg);
+  }
+  20% {
+    transform: rotate(-8deg);
+  }
+  30% {
+    transform: rotate(14deg);
+  }
+  40% {
+    transform: rotate(-4deg);
+  }
+  50% {
+    transform: rotate(10deg);
+  }
+  60% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
 
-  glowTop.value = mouseY;
-  glowLeft.value = mouseX;
+@keyframes border-animation {
+  0% {
+    top: 0;
+    left: 0;
+  }
+  25% {
+    top: 0;
+    left: 100%;
+  }
+  50% {
+    top: 100%;
+    left: 100%;
+  }
+  75% {
+    top: 100%;
+    left: 0;
+  }
+  100% {
+    top: 0;
+    left: 0;
+  }
 }
-</script>
+</style>
