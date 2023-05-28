3e5be204728ac6201caf36b35873267f32439282
fenish <sohretalhadev@gmail.com>
2023-05-28 18:49:45 +0300

    Add icons module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
   components: true,
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
